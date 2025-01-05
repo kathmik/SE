758e6044cbeb22458a9241e13a2a6323ee0e6680
kathmik <ariesgirl2535@gmail.com>
2025-01-05 14:24:41 +0100

    Update PrimeCheckTest.java

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -26,8 +26,8 @@ public class PrimeCheckTest {
     public void testPrimes() {
         /* ToDo: insert a test here */
         int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
-        for (int i = 0, i<=primeNumbers.length, i++){
-            assertEquals("Number at position " + i + " is a prime number", true, PrimeCheck.isPrime(i));
+        for (int i = 0; i<=primeNumbers.length; i++){
+            assertEquals("Number at position " + i + " is a prime number", true, PrimeCheck.isPrime(primeNumbers[i]));
         }
     }
 
@@ -37,7 +37,7 @@ public class PrimeCheckTest {
         /* ToDo: insert a test here */
         int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
         for(int i: primeNumbers){
-            for (int x = 2; x < 50; x++) {
+            for (int x = 2; i*x < 50; x++) {
                 assertEquals("Number " + i*x + " is not a prime number", false, PrimeCheck.isPrime(i*x));
             }
         }
